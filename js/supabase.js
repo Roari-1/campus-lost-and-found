d1a3f12f726fc27bd0e49b559b2c78f356f4e538
Roari-1 <rouienantiza@gmail.com>
2026-09-22 16:58:06 +0800

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,9 @@
+
+const SUPABASE_URL = "https://dxrudyrlkkvxevaiwknn.supabase.co/rest/v1/";
+
+const SUPABASE_KEY = "sb_publishable_WDqUhuE_ggUgIeQeQL6z_A_i9iJYa4O";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
